9066bacc3539ab90ae82f7ddb916d96adf64a541
Gary <qiuhuidi31@Gmail.com>
2024-09-10 11:52:40 +0800

    2024.9.10 2552

---
+++ Daily_Practice/Leecode/2552.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long countQuadruplets(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> pre(n + 1);
+    long long ans = 0;
+    for (int j = 0; j < n; ++j) {
+        int suf = 0;
+        for (int k = n - 1; k > j; --k) {
+            if (nums[j] > nums[k]) {
+                ans += static_cast<long long>(pre[nums[k]]) * suf;
+            }
+            else {
+                ++suf;
+            }
+        }
+        for (int x = nums[j] + 1; x <= n; ++x) {
+            ++pre[x];
+        }
+    }
+    return ans;
+}
+
+int main() {
+	vector nums{ 1,3,2,4,5 };
+	cout << countQuadruplets(nums) << "\n";
+}
